ba9adc9524057a57089bfd2f60dd7f6ec3fc4d0e
Alex Sheridan <alexsheridan.pro@outlook.fr>
2025-07-02 16:15:17 +0200

    2 july

--- scripts/_merging_scripts/clean_mcs2.R
+++ scripts/_merging_scripts/clean_mcs2.R
@@ -4,7 +4,6 @@ library(readr)
 library(haven)
 library(beepr)
 
-setwd("~/Desktop/EDSD project/EDSDalex/datamcs")
 
 rm(list = ls())
 gc()
@@ -12,7 +11,7 @@ gc()
 tryCatch({
 
 ### PARENT DERIVED ####
-parent_derived <- read_dta("mcs2_parent_derived.dta")
+parent_derived <- read_dta("data/datamcs/mcs2_parent_derived.dta")
 
 # Confirm that main interviewee is the mother
 table(parent_derived$BELIG00[parent_derived$BDDRES00 == 1])
@@ -33,10 +32,10 @@ parent_derived <- parent_derived %>%
   ungroup()
 
 # Save cleaned data
-write_dta(parent_derived, "clean/mcs2_parent_derived_clean.dta")
+write_dta(parent_derived, "data/datamcs/clean/mcs2_parent_derived_clean.dta")
 
 ### PARENT INTERVIEW ####
-parent_interview <- read_dta("mcs2_parent_interview.dta")
+parent_interview <- read_dta("data/datamcs/mcs2_parent_interview.dta")
 
 parent_interview_p <- parent_interview %>% select(-MCSID) %>% rename_all(~ paste0(., "_p"))
 parent_interview_m <- parent_interview %>% rename_with(~ paste0(., "_m"), .cols = -MCSID)
@@ -51,10 +50,10 @@ parent_interview <- parent_interview %>%
   slice(1) %>%
   ungroup()
 
-write_dta(parent_interview, "clean/mcs2_parent_interview_clean.dta")
+write_dta(parent_interview, "data/datamcs/clean/mcs2_parent_interview_clean.dta")
 
 ### PARENT CM INTERVIEW ####
-parent_cm_interview <- read_dta("mcs2_parent_cm_interview.dta")
+parent_cm_interview <- read_dta("data/datamcs/mcs2_parent_cm_interview.dta")
 
 parent_cm_interview_p <- parent_cm_interview %>% select(-MCSID) %>% rename_all(~ paste0(., "_p"))
 parent_cm_interview_m <- parent_cm_interview %>% rename_with(~ paste0(., "_m"), .cols = -MCSID)
@@ -69,31 +68,31 @@ parent_cm_interview <- parent_cm_interview %>%
   slice(1) %>%
   ungroup()
 
-write_dta(parent_cm_interview, "clean/mcs2_parent_cm_interview_clean.dta")
+write_dta(parent_cm_interview, "data/datamcs/clean/mcs2_parent_cm_interview_clean.dta")
 
 ### FAMILY DERIVED ####
-family_derived <- read_dta("mcs2_family_derived.dta") # one line for twins
-write_dta(family_derived, "clean/mcs2_family_derived_clean.dta")
+family_derived <- read_dta("data/datamcs/mcs2_family_derived.dta") # one line for twins
+write_dta(family_derived, "data/datamcs/clean/mcs2_family_derived_clean.dta")
 
 ### NEIGHBOURHOOD ####
-neighbourhood_observations <- read_dta("mcs2_neighbourhood_observations.dta")
-write_dta(neighbourhood_observations, "clean/mcs2_neighbourhood_observations_clean.dta")
+neighbourhood_observations <- read_dta("data/datamcs/mcs2_neighbourhood_observations.dta")
+write_dta(neighbourhood_observations, "data/datamcs/clean/mcs2_neighbourhood_observations_clean.dta")
 
 ### OLDER SIBLINGS ####
-older_siblings_questionnaire <- read_dta("mcs2_older_siblings_questionnaire.dta")
-write_dta(older_siblings_questionnaire, "clean/mcs2_older_siblings_questionnaire_clean.dta")
+older_siblings_questionnaire <- read_dta("data/datamcs/mcs2_older_siblings_questionnaire.dta")
+write_dta(older_siblings_questionnaire, "data/datamcs/clean/mcs2_older_siblings_questionnaire_clean.dta")
 
 ### PROXY PARTNER ####
-proxy_partner_interview <- read_dta("mcs2_proxy_partner_interview.dta")
-write_dta(proxy_partner_interview, "clean/mcs2_proxy_partner_interview_clean.dta")
+proxy_partner_interview <- read_dta("data/datamcs/mcs2_proxy_partner_interview.dta")
+write_dta(proxy_partner_interview, "data/datamcs/clean/mcs2_proxy_partner_interview_clean.dta")
 
 ### GEO LINKED DATA ####
-geographically_linked_data <- read_dta("mcs2_geographically_linked_data.dta")
+geographically_linked_data <- read_dta("data/datamcs/mcs2_geographically_linked_data.dta")
 names(geographically_linked_data) <- toupper(names(geographically_linked_data))
-write_dta(geographically_linked_data, "clean/mcs2_geographically_linked_data_clean.dta")
+write_dta(geographically_linked_data, "data/datamcs/clean/mcs2_geographically_linked_data_clean.dta")
 
 ### CM COGNITIVE ####
-cm_cognitive_assessment <- read_dta("mcs2_cm_cognitive_assessment.dta")
+cm_cognitive_assessment <- read_dta("data/datamcs/mcs2_cm_cognitive_assessment.dta")
 
 cm_cognitive_assessment_tw <- cm_cognitive_assessment %>% select(-MCSID) %>% rename_all(~ paste0(., "_tw"))
 cm_cognitive_assessment <- bind_cols(cm_cognitive_assessment, cm_cognitive_assessment_tw)
@@ -106,10 +105,10 @@ cm_cognitive_assessment <- cm_cognitive_assessment %>%
   slice(1) %>%
   ungroup()
 
-write_dta(cm_cognitive_assessment, "clean/mcs2_cm_cognitive_assessment_clean.dta")
+write_dta(cm_cognitive_assessment, "data/datamcs/clean/mcs2_cm_cognitive_assessment_clean.dta")
 
 ### CM ORAL ####
-cm_oral_fluid <- read_dta("mcs2_cm_oral_fluid.dta")
+cm_oral_fluid <- read_dta("data/datamcs/mcs2_cm_oral_fluid.dta")
 
 cm_oral_fluid_tw <- cm_oral_fluid %>% select(-MCSID) %>% rename_all(~ paste0(., "_tw"))
 cm_oral_fluid <- bind_cols(cm_oral_fluid, cm_oral_fluid_tw)
@@ -122,10 +121,10 @@ cm_oral_fluid <- cm_oral_fluid %>%
   slice(1) %>%
   ungroup()
 
-write_dta(cm_oral_fluid, "clean/mcs2_cm_oral_fluid_clean.dta")
+write_dta(cm_oral_fluid, "data/datamcs/clean/mcs2_cm_oral_fluid_clean.dta")
 
 ### CM INTERVIEW ####
-cm_interview <- read_dta("mcs2_cm_interview.dta")
+cm_interview <- read_dta("data/datamcs/mcs2_cm_interview.dta")
 
 cm_interview_tw <- cm_interview %>% select(-MCSID) %>% rename_all(~ paste0(., "_tw"))
 cm_interview <- bind_cols(cm_interview, cm_interview_tw)
@@ -138,10 +137,10 @@ cm_interview <- cm_interview %>%
   slice(1) %>%
   ungroup()
 
-write_dta(cm_interview, "clean/mcs2_cm_interview_clean.dta")
+write_dta(cm_interview, "data/datamcs/clean/mcs2_cm_interview_clean.dta")
 
 ### CM DERIVED ####
-cm_derived <- read_dta("mcs2_cm_derived.dta")
+cm_derived <- read_dta("data/datamcs/mcs2_cm_derived.dta")
 
 cm_derived_tw <- cm_derived %>% select(-MCSID) %>% rename_all(~ paste0(., "_tw"))
 cm_derived <- bind_cols(cm_derived, cm_derived_tw)
@@ -154,65 +153,21 @@ cm_derived <- cm_derived %>%
   slice(1) %>%
   ungroup()
 
-write_dta(cm_derived, "clean/mcs2_cm_derived_clean.dta")
-
-# ### HHGRID ###
-# hh_grid <- read_dta("mcs2_hhgrid.dta")
-# 
-# hh_grid <- hh_grid %>%
-#   mutate(momotherkids = ifelse(ahcrel == 11, 1, 0)) %>%
-#   group_by(mcsid) %>%
-#   mutate(momotherkids = max(momotherkids, na.rm = TRUE)) %>%
-#   ungroup()
-# 
-# # Gender variable
-# hh_grid <- hh_grid %>%
-#   mutate(male = ifelse(ahcsex00 == 1, 1, 0)) %>%
-#   group_by(mcsid) %>%
-#   mutate(male = max(male, na.rm = TRUE)) %>%
-#   ungroup()
-# 
-# # Keep main respondent and partner
-# hh_grid <- hh_grid %>% filter(aelig == 1 | aelig == 2)
-# 
-# # Duplicate all variables for second parent
-# hh_grid_p <- hh_grid %>% select(apnum00:ahinca00) %>% rename_all(~ paste0(., "_p"))
-# hh_grid <- bind_cols(hh_grid, hh_grid_p)
-# 
-# # Make first set valid for first parent, second for partner
-# hh_grid <- hh_grid %>%
-#   mutate(across(apnum00:ahinca00, ~ ifelse(BELIG00 != 1, NA, .))) %>%
-#   mutate(across(ends_with("_p"), ~ ifelse(BELIG00 == 1, NA, .)))
-# 
-# # Pull partner values into first record
-# hh_grid <- hh_grid %>%
-#   group_by(mcsid) %>%
-#   mutate(across(ends_with("_p"), ~ ifelse(is.na(.), lead(.), .))) %>%
-#   ungroup()
-# 
-# # Keep only first record per mcsid
-# hh_grid <- hh_grid %>% group_by(mcsid) %>% slice(1) %>% ungroup()
-# 
-# # Save cleaned data
-# write_dta(hh_grid, "mcs_cm_hhgrid_clean.dta")
-
-# # Convert strings to numeric
-# parent_interview <- parent_interview %>%
-#   mutate(across(where(is.character), ~ as.numeric(as.character(.)), .names = "{.col}_num"))
+write_dta(cm_derived, "data/datamcs/clean/mcs2_cm_derived_clean.dta")
 
 ### MERGE FILES ####
-parent_derived <- read_dta("clean/mcs2_parent_derived_clean.dta")
-parent_interview <- read_dta("clean/mcs2_parent_interview_clean.dta")
-parent_cm_interview <- read_dta("clean/mcs2_parent_cm_interview_clean.dta")
-family_derived <- read_dta("clean/mcs2_family_derived_clean.dta")
-neighbourhood_observations <- read_dta("clean/mcs2_neighbourhood_observations_clean.dta")
-older_siblings_questionnaire <- read_dta("clean/mcs2_older_siblings_questionnaire_clean.dta")
-proxy_partner_interview <- read_dta("clean/mcs2_proxy_partner_interview_clean.dta")
-geographically_linked_data <- read_dta("clean/mcs2_geographically_linked_data_clean.dta")
-cm_cognitive_assessment <- read_dta("clean/mcs2_cm_cognitive_assessment_clean.dta")
-cm_oral_fluid <- read_dta("clean/mcs2_cm_oral_fluid_clean.dta")
-cm_interview <- read_dta("clean/mcs2_cm_interview_clean.dta")
-cm_derived <- read_dta("clean/mcs2_cm_derived_clean.dta")
+parent_derived <- read_dta("data/datamcs/clean/mcs2_parent_derived_clean.dta")
+parent_interview <- read_dta("data/datamcs/clean/mcs2_parent_interview_clean.dta")
+parent_cm_interview <- read_dta("data/datamcs/clean/mcs2_parent_cm_interview_clean.dta")
+family_derived <- read_dta("data/datamcs/clean/mcs2_family_derived_clean.dta")
+neighbourhood_observations <- read_dta("data/datamcs/clean/mcs2_neighbourhood_observations_clean.dta")
+older_siblings_questionnaire <- read_dta("data/datamcs/clean/mcs2_older_siblings_questionnaire_clean.dta")
+proxy_partner_interview <- read_dta("data/datamcs/clean/mcs2_proxy_partner_interview_clean.dta")
+geographically_linked_data <- read_dta("data/datamcs/clean/mcs2_geographically_linked_data_clean.dta")
+cm_cognitive_assessment <- read_dta("data/datamcs/clean/mcs2_cm_cognitive_assessment_clean.dta")
+cm_oral_fluid <- read_dta("data/datamcs/clean/mcs2_cm_oral_fluid_clean.dta")
+cm_interview <- read_dta("data/datamcs/clean/mcs2_cm_interview_clean.dta")
+cm_derived <- read_dta("data/datamcs/clean/mcs2_cm_derived_clean.dta")
 
 mcs2 <- full_join(parent_derived, parent_interview, by = "MCSID")
 mcs2 <- full_join(mcs2, parent_cm_interview, by = "MCSID")
@@ -229,7 +184,7 @@ mcs2 <- full_join(mcs2, cm_derived, by = "MCSID")
 mcs2 <- mcs2 %>%
   select(-ends_with(".x"), -ends_with(".y"))
 
-write_dta(mcs2, "clean/mcs2_clean.dta")
+write_dta(mcs2, "data/datamcs/clean/mcs2_clean.dta")
 
 }, checkerror = function(e) {
   beep(8)  # Play an alert sound on error
