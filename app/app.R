628cb0280e8da7a4fbe780e62c3a5bc897158dea
Alex Sheridan <alexsheridan.pro@outlook.fr>
2025-07-24 09:21:32 +0200

    shiny

--- app/app.R
+++ app/app.R
@@ -2,7 +2,7 @@
 library(shiny)
 library(stargazer)
 
-load("./app/data/models_lists.RData")
+load("./data/models_lists.RData")
 
 # Replace with your actual model list
 modellingfr_models_list <- list(
